f73d40fb7c11a387a0313766e14f533158204e15
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-06 13:56:56 +0530

    handleDragEnd added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React,{useState} from 'react';
 import initialData from "./initial-data"
-import {DragDropContext,Droppable,Draggable} from "react-beautiful-dnd";
+import {DragDropContext,} from "react-beautiful-dnd";
 import Column from "./Components/Column";
 
 
 const App = () => {
   const [state, setState] = useState(initialData);
 
+  const handleDragEnd = (result) =>{
+      console.log(result)
+
+  }
+
 
 
     return (
-     <div>
+    
+         <DragDropContext onDragEnd={handleDragEnd}>
          {state.columnOrder.map((columnId) =>{
             const column =state.columns[columnId];
             const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
@@ -18,10 +24,11 @@ const App = () => {
 
             return (<Column key ={columnId} column={column} tasks={tasks}/>);
          })}
+         </DragDropContext>
              
 
 
-     </div>
+     
     )
 }
 
